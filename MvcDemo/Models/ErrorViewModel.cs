222b77e445025bc33c73eab35c8dcaeb6813693b
Milen Ivanov <milensl92@gmail.com>
2021-09-29 00:00:27 +0300

    Created ASP.Net-MVC-project with IdentityPages and added ADMIN-LTE

---
+++ MvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace MvcDemo.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
